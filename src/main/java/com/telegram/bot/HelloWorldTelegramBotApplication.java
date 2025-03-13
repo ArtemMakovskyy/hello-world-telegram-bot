b923b259bfb140e28cc7928046948ae0e40a1f24
Artem Makovskyy <photomak79@gmail.com>
2025-03-13 16:29:02 +0200

    init

---
+++ src/main/java/com/telegram/bot/HelloWorldTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.telegram.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldTelegramBotApplication.class, args);
+	}
+
+}
